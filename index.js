2d52bffda43d59aebdd1bd28edfa2514fa1b7738
RajKKapadia <raajforyou@gmail.com>
2019-08-21 13:54:12 +0530

    Trying to run it on heroku

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+
+var app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at ${PORT}`);
+});
