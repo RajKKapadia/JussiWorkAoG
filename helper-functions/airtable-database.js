1a6b8ab8514c2d7855ac222ab52c50b820513c77
RajKKapadia <raajforyou@gmail.com>
2019-08-23 16:42:34 +0530

    Completed work as per talk.

--- helper-functions/airtable-database.js
+++ helper-functions/airtable-database.js
@@ -7,26 +7,128 @@ const API_KEY = process.env.API_KEY;
 // Get the Last Question Asked and the Level
 const getLQALevel = async (studentName) => {
 
-    url = 'https://api.airtable.com/v0/'+APP_ID+'/tblOwl9fQunAzEa4t?view=Grid%20view&filterByFormula=(AND({Name}="'+studentName+'"))&maxRecords=1';
+    url = 'https://api.airtable.com/v0/'+APP_ID+'/Student?view=Grid%20view&filterByFormula=(AND({Name}="'+studentName+'"))&maxRecords=1';
     headers = {
         Authorization: 'Bearer '+API_KEY
     }
     
     const response = await axios.get(url, {headers});
-
     let id = response.data.records[0]['id'];
     let ID = response.data.records[0]['fields']['ID'];
+    let Name = response.data.records[0]['fields']['Name'];
     let Level = response.data.records[0]['fields']['Level'];
     let ImageLQA = response.data.records[0]['fields']['ImageLQA'];
-
+    
     return {
         'id': id,
         'ID': ID,
+        'Name': Name,
         'Level': Level,
         'ImageLQA': ImageLQA
-    };
+    }
+};
+
+// Get the new question on LQA
+const getNewQuestion = async (lqa) => {
+
+    url = 'https://api.airtable.com/v0/'+APP_ID+'/ImageQuestions?view=Grid%20view&filterByFormula=(AND({QuestionID}="'+lqa+'"))&maxRecords=1';
+    headers = {
+        Authorization: 'Bearer '+API_KEY
+    }
+
+    const response = await axios.get(url, {headers});
+
+    if (response.data.records.length == 0) {
+        return 0;
+    } else {
+        let QuestionID = response.data.records[0]['fields']['QuestionID'];
+        let Hint = response.data.records[0]['fields']['Hint'];
+        let Answer = response.data.records[0]['fields']['Answer'];
+        let Question = response.data.records[0]['fields']['Question'];
+        let Difficulty = response.data.records[0]['fields']['Difficulty'];
+        let ImageURL = response.data.records[0]['fields']['Image'][0]['thumbnails']['small']['url'];
+
+        return {
+            'QuestionID': QuestionID,
+            'Hint': Hint,
+            'Answer': Answer,
+            'Question': Question,
+            'Difficulty': Difficulty,
+            'ImageURL': ImageURL
+        }
+    }
+};
+
+// Update the student data
+const updateStudent = async (studentID, fields) => {
+    url = `https://api.airtable.com/v0/${APP_ID}/Student/${studentID}`;
+    headers = {
+        'Authorization': 'Bearer '+API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    const response = await axios.patch(url, {fields}, {headers});
+
+    if (response.status == 200) {
+        return 1;
+    } else {
+        return 0;
+    }
+};
+
+// Create ImageQuestionProgress
+const createImageQuestionProgress = async (fields) => {
+    url = `https://api.airtable.com/v0/${APP_ID}/ImageQuestionsProgress`;
+    headers = {
+        'Authorization': 'Bearer '+API_KEY,
+        'Content-Type': 'application/json'
+    }
+    
+    const response = await axios.post(url, {fields}, {headers});
+
+    if (response.status == 200) {
+        return 1;
+    } else {
+        return 0;
+    }
+};
+
+// Get Image Question Progress data
+const getImageQuestionProgressID = async (QID, Name) => {
+    url = 'https://api.airtable.com/v0/'+APP_ID+'/ImageQuestionsProgress?view=Grid%20view&filterByFormula=(AND({QuestionID}="'+QID+'", {Name}="'+Name+'"))&maxRecords=1';
+    headers = {
+        Authorization: 'Bearer '+API_KEY
+    }
+
+    const response = await axios.get(url, {headers});
+
+    let id = response.data.records[0].id;
+
+    return id;
+};
+
+// Update Image Question Progress
+const updateImageQuestionProgress = async (id, fields) => {
+    url = `https://api.airtable.com/v0/${APP_ID}/ImageQuestionsProgress/${id}`;
+    headers = {
+        'Authorization': 'Bearer '+API_KEY,
+        'Content-Type': 'application/json'
+    }
+
+    const response = await axios.patch(url, {fields}, {headers});
+
+    if (response.status == 200) {
+        return 1;
+    } else {
+        return 0;
+    }
 };
 
 module.exports = {
-    getLQALevel
+    getLQALevel,
+    getNewQuestion,
+    updateStudent,
+    createImageQuestionProgress,
+    getImageQuestionProgressID,
+    updateImageQuestionProgress
 }
--- index.js
+++ index.js
@@ -3,22 +3,264 @@ const bodyParser = require('body-parser');
 
 const ad = require('./helper-functions/airtable-database');
 
-const app = express();
+// Actions-on-Google
+const {
+    dialogflow,
+    actionssdk,
+    Image,
+    Table,
+    Carousel,
+    Suggestions,
+    BasicCard
+  } = require('actions-on-google');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+const app = dialogflow({
+    debug: true
+});
+
+// Error handling
+app.catch((conv, error) => {
+    console.error(error);
+    conv.ask('I encountered a glitch. Can you say that again?');
+});
+
+// Fallback
+app.fallback((conv) => {
+    conv.ask(`I couldn't understand. Can you say that again?`);
+});
+
+// Step - 1 User provides name
+app.intent('provides.name', async (conv, params) => {
+    let studentName = params.KnownStudentName;
+    
+    try {
+        let record = await ad.getLQALevel(studentName);
+        conv.data.studentName = studentName;
+        conv.data.studentID = record['id'];
+        conv.data.LQA = record['ImageLQA'];
+        conv.data.Level = record['Level'];
+
+        conv.contexts.set('await_quiz_type',1);
+
+        // Set right answer count
+        let count = 0;
+        conv.data.count = count;
+
+        conv.ask(`Hello ${studentName}, What would you like to have today?`)
+        conv.ask(new Suggestions('Word', 'Clock', 'Maths'));
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Step - 2 Choice is WORD
+app.intent('choice.WORD', async (conv) => {
+    let lqa = conv.data.LQA;
+    try {
+        let record = await ad.getNewQuestion(lqa+1);
+        let Hint = record['Hint'];
+        let Answer = record['Answer'];
+        let Question = record['Question'];
+        let ImageURL = record['ImageURL'];
+        let QID = record['QuestionID'];
+        conv.contexts.set('await_word_answer',1);
+        conv.data.Hint = Hint;
+        conv.data.Answer = Answer;
+        conv.data.Question = Question;
+        conv.data.ImageURL = ImageURL;
+        conv.data.QID = QID;
+
+        conv.ask(Question);
+        conv.ask(new BasicCard({
+            image: new Image({
+                url: ImageURL,
+                alt: 'Internet Error'
+            })
+        }))
+
+    } catch (error) {
+        console.log(error);
+    }
+});
+
+// Step - 3 User provides the answer
+app.intent('provides.WORDANSWER', async (conv) => {
+    let clappURL = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/emojidex/59/clapping-hands-sign_emoji-modifier-fitzpatrick-type-5_1f44f-1f3fe_1f3fe.png';
+    let actualAnswer = conv.data.Answer;
+    let userAnswer = conv.query;
+
+    // Create new record for ImageQuestionProgress
+    fields = {
+        'Name': conv.data.studentName,
+        'QuestionID': conv.data.QID,
+        'Answered': 1,
+        'Answer1': userAnswer,
+        'Answer2': 'NA'
+    }
+
+    console.log('Inserted question id --> ', conv.data.QID);
+
+    ad.createImageQuestionProgress(fields).then((flag) => {
+        console.log('Created IQP --> ', flag);
+    }).catch((error) => {
+        console.log('Error at Create IQP --> ', error);
+    });
+
+    if (actualAnswer.toLowerCase() === userAnswer.toLowerCase()) {
+
+        let ssml = '<speak>'+
+            'That is correct answer.'+
+            '<audio src="https://www.soundjay.com/human/applause-01.mp3"></audio>'+
+            '</speak>';
+        conv.ask(ssml);
+        conv.ask(new BasicCard({
+            image: new Image({
+                url: clappURL,
+                alt: 'Internet Error'
+            })
+        }));
+
+        // Increment the count of right answer
+        conv.data.count = conv.data.count+1;
+
+        // Update the Student --> ImageLQA with conv.data.LQA
+        conv.data.LQA = conv.data.LQA+1;
+        fields = {
+            'ImageLQA': conv.data.LQA
+        }
+
+        let flag = await ad.updateStudent(conv.data.studentID, fields)
+
+        if (flag == 1) {
+            // Ask new question here
+            conv.contexts.set('await_continue', 1);
+            conv.ask('Do you want new question?');
+            conv.ask(new Suggestions('Yes', 'No'));    
+        }
+
+    } else {
+        // Reset the count to zero for Level
+        conv.data.count = conv.data.count-1;
+
+        conv.contexts.set('await_word_answer_2',1);
+
+        let ssml = '<speak>'+
+            '<audio src="https://www.soundjay.com/misc/fail-buzzer-01.mp3"></audio>'+
+            '<break time="200ms"/>'+
+            conv.data.Hint+
+            '</speak>';
+        conv.ask(ssml);
+        conv.ask(new BasicCard({
+            image: new Image({
+                url: conv.data.ImageURL,
+                alt: 'Internet Error'
+            })
+        }))
+    }
+});
+
+// User provides answer second time
+app.intent('provides.WORDANSWER2', async (conv) => {
+    let clappURL = 'https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/emojidex/59/clapping-hands-sign_emoji-modifier-fitzpatrick-type-5_1f44f-1f3fe_1f3fe.png';
+    let actualAnswer = conv.data.Answer;
+    let userAnswer = conv.query;
+
+    // Update ImageQuestionProgress
+    fields = {
+        'Answer2': userAnswer
+    }
+
+    let id = await ad.getImageQuestionProgressID(conv.data.QID, conv.data.studentName);
+
+    console.log('Updated question id --> ', conv.data.QID);
+
+    ad.updateImageQuestionProgress(id, fields).then((flag) => {
+        console.log('Updated IQP --> ', flag);
+    }).catch((error) => {
+        console.log('Error at UIPQ --> ', error);
+    })
+
+    if (actualAnswer.toLowerCase() === userAnswer.toLowerCase()) {
+
+        let ssml = '<speak>'+
+            'That is correct answer.'+
+            '<audio src="https://www.soundjay.com/human/applause-01.mp3"></audio>'+
+            '</speak>';
+        conv.ask(ssml);
+        conv.ask(new BasicCard({
+            image: new Image({
+                url: clappURL,
+                alt: 'Internet Error'
+            })
+        }));
+        // Increase the count for Level
+        conv.data.count = conv.data.count+1;
+
+        // Update the Student --> ImageLQA with conv.data.LQA
+        conv.data.LQA = conv.data.LQA+1;
+        fields = {
+            'ImageLQA': conv.data.LQA
+        }
+
+        let flag = await ad.updateStudent(conv.data.studentID, fields)
+
+        if (flag == 1) {
+            // Ask new question here
+            conv.contexts.set('await_continue', 1);
+            conv.ask('Do you want new question?');
+            conv.ask(new Suggestions('Yes', 'No'));    
+        }
+
+    } else {
+        // Reset the count to zero for Level
+        conv.data.count = conv.data.count-1;
+
+        conv.data.LQA = conv.data.LQA+1;
+
+        fields = {
+            'ImageLQA': conv.data.LQA
+        }
+
+        let flag = await ad.updateStudent(conv.data.studentID, fields)
+
+        if (flag == 1) {
+            // Ask new question here
+            conv.contexts.set('await_continue', 1);
+            conv.ask(`Not quite. The answer is ` + actualAnswer + `. Do you want another word?`);
+            conv.ask(new Suggestions('Yes', 'No'));   
+        }
+    }
+});
+
+// User wants to continue to next question
+app.intent('choice.CONTINUE.YES', async (conv) => {
+
+    console.log('The count is --> ', conv.data.count);
+    console.log('The LQA is --> ', conv.data.LQA);
+    
+    conv.contexts.set('await_quiz_type',1);
+
+    if (conv.data.count % 3 == 0) {
+        conv.ask('Congratulation, you cleared the level.');
+    }
+    conv.ask('What would you like to have?');
+    conv.ask(new Suggestions('Word', 'Clock', 'Maths'));
+});
+
+// Webserver
+const Webapp = express();
+
+Webapp.use(bodyParser.urlencoded({ extended: true }));
+Webapp.use(bodyParser.json());
 
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    ad.getLQALevel('Alice').then((record) => {
-        console.log(record);
-        res.send(record);
-    }).catch((e) => {
-        console.log(e);
-    });
+Webapp.get('/', (req, res) => {
+    res.send('Hello World.!')
 });
 
-app.listen(PORT, () => {
+Webapp.post('/webhook', app);
+
+Webapp.listen(PORT, () => {
     console.log(`Server is running at ${PORT}`);
 });
